d3807daec75abc1debfd777c8a64416b0229dfac
Omar Ahmad <dev.omar.taher@gmail.com>
2023-07-31 17:00:47 +0300

    Completed stage 3/6

--- true_or_false.sh
+++ true_or_false.sh
@@ -1,12 +1,15 @@
 #!/user/bin/env bash
 
 credentials_filename=ID_card.txt
+
 echo 'Welcome to the True or False Game!'
 curl --silent --output "$credentials_filename" http://127.0.0.1:8000/download/file.txt
 
 username=$(cut -d '"' -f 4 "$credentials_filename")
 password=$(cut -d '"' -f 8 "$credentials_filename")
 cookie_filename=cookie.txt
-curl --silent --output "$cookie_filename" --user "$username":"$password" http://127.0.0.1:8000/login
+curl --silent --output "$cookie_filename" --cookie-jar 'login-cookie' --user "$username":"$password" http://127.0.0.1:8000/login
+printf 'Login message: %s\n' "$(cat "$cookie_filename")"
 
-printf 'Login message: %s' "$(cat "$cookie_filename")"
+response=$(curl --cookie 'login-cookie' http://127.0.0.1:8000/game)
+printf "Response: %s" "$response"
